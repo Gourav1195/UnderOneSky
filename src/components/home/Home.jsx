528fe6ef6a2eba6962572eb9888e560b6db923f9
Gourav Modi <gouravmodi1195@gmail.com>
2024-12-16 02:23:01 +0530

    homeUpdates

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,29 @@
 import LunarApp from "./LunarApp"
 import WeatherApp from "./WeatherApp"
-import {
-  // NasaImg,
-  UnsplashImage
-  } from "./NasaImg"
+
 import { useTheme } from "../theme"
 
 const Home = () => {
     const {theme} = useTheme;
   return (
+
     <div className={`page${theme}`}>
+            {/* <img className="banner_image" src="/banner.jpg" alt="night sky"  /> */}
+      <div id="top" class="banner_image bg">
+        <div class="follow_container">
+            <div class="col-md-6 col-md-offset-3">
+                <h2 class="top-title">Under One Sky
+
+                </h2>
+                <h2 class="white second-title">"lorem ipsum "</h2>
+                
+            </div>
+        </div>
+      </div>
+      
+<div>
         <WeatherApp />
-        <LunarApp />
-        <UnsplashImage />
-        {/* <img src="https://source.unsplash.com/1400x900/?stars" alt="night sky" /> */}
+        <LunarApp /></div>
     </div>
   )
 }
